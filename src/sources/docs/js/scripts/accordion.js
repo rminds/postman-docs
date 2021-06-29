5fd5e475f4a8f2e041a4d3394ed0c55a12813807
Larenkosha <larenkosha@gmail.com>
2021-06-29 23:11:36 +0300

    fix elements

--- src/sources/docs/js/scripts/accordion.js
+++ src/sources/docs/js/scripts/accordion.js
@@ -1,11 +1,20 @@
 $.fn.accordion = function() {
     let accordion = ($el) => {
+        let with_inner = $el.hasClass('with_inner');
+
         this.active = function () {
             $el.on('click', '.accordion-head', function(){
-                let $parent = $(this).parent('.accordion-item');
+                if ($(this).hasClass('inner')) {
+                    let $parent = $(this).parent('.accordion-item.inner');
 
-                $parent.hasClass('active') ? $parent.removeClass('active') : $parent.addClass('active').siblings().removeClass('active');
-            })
+                    $parent.hasClass('active') ? $parent.removeClass('active') : $parent.addClass('active').siblings().removeClass('active');
+                } else {
+                    let $parent = $(this).parent('.accordion-item');
+
+                    $parent.hasClass('active') ? $parent.removeClass('active') : $parent.addClass('active').siblings().removeClass('active');
+                }
+                
+            });
         }
 
         this.init = function () {
